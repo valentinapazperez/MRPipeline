18783be02be5b7a73a531999aa021d3a42613acc
Antoine S <antoine.salzmann.14@ucl.ac.uk>
2021-07-21 11:28:48 +0100

    Adding bgen output option

--- Extract_SNPs_18.06.21.sh
+++ Extract_SNPs_18.06.21.sh
@@ -17,47 +17,49 @@ output_name=extracted_snps
 while getopts "s:bgo:n:" flag
 do
 
-       	case "${flag}" in
-               	s) snp_list=${OPTARG} #enter snplist location
-                       	;;
-               	b) bgen=1 #output bgen file
-                       	;;
-               	g) gen=1 #output gen file
-                       	;;
-               	o) out_dir=${OPTARG} #set output directory
-                       	;;
-               	n) output_name=${OPTARG} #set file name
-                       	;;
-               	\?) echo "ERROR: Invalid option: -$OPTARG"
-                       	exit 1
-                       	;;
-               	:) echo "ERROR: Option -$OPTARG requires an argument."
-                       	exit 1
-                       	;;
-                       	
-                esac 
-                
+        case "${flag}" in
+                s) snp_list=${OPTARG} #enter snplist location
+                        ;;
+                b) bgen=1 #output bgen file
+                        ;;
+                g) gen=1 #output gen file
+                        ;;
+                o) out_dir=${OPTARG} #set output directory
+                        ;;
+                n) output_name=${OPTARG} #set file name
+                        ;;
+                \?) echo "ERROR: Invalid option: -$OPTARG"
+                        exit 1
+                        ;;
+                :) echo "ERROR: Option -$OPTARG requires an argument."
+                        exit 1
+                        ;;
+
+                esac
+
 done
+
 
 #Make output directory if required. Doesnt thow error if not present
 mkdir -p $out_dir
 
 #If asking for genotype output
 if [ $gen = 1 ]; then
-       	#Loop through chomosomes in QC data
-       	for i in {1..22}; do
-               	/shared/ucl/apps/bgen/1.1.4/bin/bgenix \
-               	-g ${UKB_QC}/C${i}_ukbb_v3_eur_indiv_variant_qc.bgen \
-               	-incl-rsids $snp_list | \
+        echo "Outputting as gen file..."
+        #Loop through chomosomes in QC data
+        for i in {1..22}; do
+                /shared/ucl/apps/bgen/1.1.4/bin/bgenix \
+                -g ${UKB_QC}/C${i}_ukbb_v3_eur_indiv_variant_qc.bgen \
+                -incl-rsids $snp_list | \
 
-               	#pipe to qctool to convert to gen
-               	/shared/ucl/apps/qctool/ba5eaa44a62f/bin/qctool_v2.0.1 -g - -filetype bgen \
-               	-og ${out_dir}/CHR${i}_extracted_snps.gen
+                #pipe to qctool to convert to gen
+                /shared/ucl/apps/qctool/ba5eaa44a62f/bin/qctool_v2.0.1 -g - -filetype bgen \
+                -og ${out_dir}/CHR${i}_extracted_snps.gen
 
-               	#concatenate gen files into merged file
-               	if [ -f ${out_dir}/CHR${i}_extracted_snps.gen ] ; then
-                       	cat ${out_dir}/CHR${i}_extracted_snps.gen >> ${out_dir}/extracted_SNPs_plus_col.gen
-                       	awk '{$2=""; print $0}' ${out_dir}/extracted_SNPs_plus_col.gen >> ${out_dir}/${output_name}.gen
+                #concatenate gen files into merged file
+                if [ -f ${out_dir}/CHR${i}_extracted_snps.gen ] ; then
+                        cat ${out_dir}/CHR${i}_extracted_snps.gen >> ${out_dir}/extracted_SNPs_plus_col.gen
+                        awk '{$2=""; print $0}' ${out_dir}/extracted_SNPs_plus_col.gen >> ${out_dir}/${output_name}.gen
 
                 #Remove irrelevant files
                 rm ${out_dir}/extracted_SNPs_plus_col.gen
@@ -66,3 +68,28 @@ if [ $gen = 1 ]; then
                 fi
         done
 fi
+
+if [ $bgen = 1 ]; then 
+        echo "Outputting as bgen file..."
+        #Loop extracting SNPs from SNP list as bgen 
+        for i in {1..22}; do 
+                echo "Extracting SNPs from chromosome ${i}..."
+                #Extract the SNPs as bgen 
+                /shared/ucl/apps/bgen/1.1.4/bin/bgenix \
+                -g ${UKB_QC}/C${i}_ukbb_v3_eur_indiv_variant_qc.bgen \
+                -incl-rsids $snp_list > ${out_dir}/CHR${i}_extracted_snps.bgen
+                
+                
+        done
+        
+        #Concatinate Bgen files 
+        echo "Concatinating bgen files into single output..."
+        /shared/ucl/apps/bgen/1.1.4/bin/cat-bgen \
+        -clobber -g ${out_dir}/CHR{1..22}_extracted_snps.bgen \
+        -og ${out_dir}/${output_name}.bgen
+        
+        #Remove intermediate files 
+        echo "Removing intermediate files..."
+        rm ${out_dir}/CHR{1..22}_extracted_snps.bgen
+        
+fi 
